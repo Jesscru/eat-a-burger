0ebcdb2a5a2d66f84e6873a469244d63e6bd43ee
jesscru <jessicacrump7@gmail.com>
2020-12-10 23:24:34 -0700

    added schema and seeds to sql files; required express in server.js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("BBQ Burger", 'false');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Black and Bleu", 'false');
+
+INSERT INTO items (item_name, price)
+VALUES ("Mushroom and Swiss", 'true');
