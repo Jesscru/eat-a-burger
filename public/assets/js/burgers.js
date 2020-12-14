82c9a9ed86e6174ab4a31885c54b003f019f14fe
jesscru <jessicacrump7@gmail.com>
2020-12-13 21:17:12 -0700

    added button functionality to Devour it buttons and got data to display

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -8,12 +8,19 @@ $(function() {
         devoured: newburger
       };
   
+      var checkDev = {
+        devoured: $("[devoured=0]:checked").val()
+      };
+
       // Send the PUT request.
       $.ajax("/api/burgers/" + id, {
         type: "PUT",
         data: newDevoured
       }).then(
         function() {
+          if (!checkDev) {
+            $(".change-devoured").css('visibility', 'hidden')
+          }
           console.log("changed devoured to", newburger);
           // Reload the page to get the updated list
           location.reload();
@@ -24,12 +31,12 @@ $(function() {
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
+
       var newBurger = {
         name: $("#burg").val().trim(),
         devoured: $("[devoured=0]:checked").val().trim()
       };
-  
+      
       // Send the POST request.
       $.ajax("/api/burgers", {
         type: "POST",
